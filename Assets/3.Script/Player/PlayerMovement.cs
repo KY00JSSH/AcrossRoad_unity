b879e451c413018307762510ccdb03fdba263410
전홍현 <toinbee3@naver.com>
2024-06-27 19:40:57 +0900

    [Feat] CinemaChine Camera Chooka
    시네마신 추가. Movement 변수 moveSpeed, moveDistance 추가.

--- Assets/3.Script/Player/PlayerMovement.cs
+++ Assets/3.Script/Player/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     private Rigidbody playerRigid;
     private Vector3 targetPosition;
+    private float moveDistance = 2f, moveSpeed = 15f;
+
     private void Awake() {
         TryGetComponent(out playerRigid);
     }
@@ -15,21 +17,26 @@ public class PlayerMovement : MonoBehaviour {
     private void Update() {
         
         // WASD or 방향키 입력에 따라 방향으로 이동
-        // WASD 방향으로 쳐다보고(회전), 그 방향으로 설정 거리만큼 이동
+        // WASD 방향으로 쳐다보고(회전), 그 방향으로 moveDistance 만큼 이동
 
         if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            targetPosition = transform.position + transform.forward * moveDistance;
         }
-        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+        else if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
             transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+            targetPosition = transform.position + transform.forward * moveDistance;
         }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
             transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            targetPosition = transform.position + transform.forward * moveDistance;
         }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
             transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+            targetPosition = transform.position + transform.forward * moveDistance;
         }
 
-        //transform.position = Vector3.MoveTowards(transform.position, Vector3.forward * 5, 2);
+        // targetPosition ( forward * moveDistace) 를 향해 moveSpeed 속도로 이동
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
 }
