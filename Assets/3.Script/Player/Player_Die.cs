cb6f0e5f5a0ad5ade9d84115a93bfdbce409f848
김수주 <tnwn1122@naver.com>
2024-06-28 19:08:09 +0900

    [Update] player die시 die 애니메이션 위치 동기화

--- Assets/3.Script/Player/Player_Die.cs
+++ Assets/3.Script/Player/Player_Die.cs
@@ -7,13 +7,18 @@ public class Player_Die : MonoBehaviour
 
     public ParticleSystem dieParticle;
     private PlayerControll playerControll;
+    [SerializeField] GameObject activeObj;
+
+
     private void Start()
     {
-        dieParticle.transform.position = transform.position;
 
         playerControll = GetComponentInChildren<PlayerControll>();
         if (playerControll != null)
         {
+            // 플레이어가 죽으면 활성화 되어있는 오브젝트를 찾아 위치 동기화
+            playerControll.OnDead += Find_Active_Object;
+            // 플레이어가 죽으면 파티클 활성화하는 코루틴 실행
             playerControll.OnDead += Player_Die_Check;
         }
         else
@@ -21,6 +26,19 @@ public class Player_Die : MonoBehaviour
             Debug.Log("playerControll null");
         }
 
+    }
+
+    
+    public void Find_Active_Object()
+    {
+        // 상위 오브젝트에서 활성화되어있는 오브젝트를 찾음
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.activeSelf && child.gameObject.CompareTag("Player"))
+            {
+                dieParticle.transform.position = child.transform.position;
+            }
+        }
     }
 
     public void Player_Die_Check()
@@ -31,10 +49,8 @@ public class Player_Die : MonoBehaviour
 
     public IEnumerator DieParticle()
     {
-        Debug.Log("들어오나?");
         dieParticle.gameObject.SetActive(true);
         yield return new WaitForSeconds(1f);
-        Debug.Log("안들어오나?");
         dieParticle.gameObject.SetActive(false);
     }
 }
