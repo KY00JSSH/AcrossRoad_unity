84afb8dbee1c1577ab030c3642c57ca7bcbe3362
김수진 <nuu042@naver.com>
2024-06-28 13:26:00 +0900

    [Feat] 주변 장애물 삭제 스킬 추가, 새 장애물 생성 딜레이 추가해야함

--- Assets/3.Script/Rocks/RocksSpawner.cs
+++ Assets/3.Script/Rocks/RocksSpawner.cs
@@ -6,23 +6,23 @@ public class RocksSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] rocks;             //기본 rocks prefabs
+    public List<GameObject> rocksList;     //pooling list
 
-    private List<GameObject> rocksList;     //pooling list
-
-    GameObject player;                      //현재 player
+    private GameObject player;              //현재 player
     public float range = 30f;              //랜덤 drop 반경 설정
+    public bool isDrop = false;
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-
         rocksList = new List<GameObject>();
+        isDrop = false;
 
         for (int i = 0; i < 5; i++)
         {
-            for(int j = 0; j < 7; j++)
+            for (int j = 0; j < 7; j++)
             {
-                GameObject rock= Instantiate(rocks[i]); //5개 prefabs 5개씩 list에 담음
+                GameObject rock = Instantiate(rocks[i]); //5개 prefabs 5개씩 list에 담음
                 rock.SetActive(false);
                 rocksList.Add(rock);
             }
@@ -32,21 +32,20 @@ public class RocksSpawner : MonoBehaviour
     void Start()
     {
         Vector3 playerPosition = player.transform.position;
-
-        if (Vector3.Distance(transform.position, playerPosition)  <= range)  // player 현재 position range 범위 안에
-        {
-            DropRocksAroundPlayer(playerPosition);
-        }
+        DropRocksAroundPlayer(playerPosition);
     }
 
-    private void DropRocksAroundPlayer(Vector3 playerPosition)
+    public void DropRocksAroundPlayer(Vector3 playerPosition)
     {
-        for(int i = 0; i< 10; i ++)
+        if (Vector3.Distance(transform.position, playerPosition) <= range)  // player 현재 position range 범위 안에
         {
-            GameObject rock = rocksList[Random.Range(0, rocksList.Count)];
-            Vector3 randomPosition = GetRandomPosition(playerPosition);
-            rock.transform.position = randomPosition;
-            rock.SetActive(true);
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject rock = rocksList[Random.Range(0, rocksList.Count)];
+                Vector3 randomPosition = GetRandomPosition(playerPosition);
+                rock.transform.position = randomPosition;
+                rock.SetActive(true);
+            }
         }
     }
 
