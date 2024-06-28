89c06ba66265bf1b1092480c44d6749839ce112b
김수진 <nuu042@naver.com>
2024-06-28 14:08:54 +0900

    [Update] GameManager 싱글턴 수정하기

--- Assets/3.Script/GameManager.cs
+++ Assets/3.Script/GameManager.cs
@@ -4,34 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    //싱글톤 1번째 형태 - (장점) 인스턴스 보호
-    private static GameManager instance = null;
-    public static GameManager Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = FindObjectOfType<GameManager>();
-            }
-            return instance;
-        }
-    }
+    public static GameManager instance = null;
 
     private void Awake()
     {
-        if (Instance == null)
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
         {
             Destroy(gameObject);
+            //return;
         }
+
     }
+
     public bool isGameover { get; private set; }
     public int Score = 0;
-
-    private void Start()
-    {
-
-    }
 
     public void EndGame()
     {
