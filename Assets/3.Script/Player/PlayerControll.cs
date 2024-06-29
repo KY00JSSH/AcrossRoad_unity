05ff2cf696f001404af6dceb66578811bf68422a
김수주 <tnwn1122@daum.net>
2024-06-30 00:54:56 +0900

    [Update] 스킬 버튼 추가

--- Assets/3.Script/Player/PlayerControll.cs
+++ Assets/3.Script/Player/PlayerControll.cs
@@ -2,21 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControll : MonoBehaviour
 {
     /*
-     *     1. 생존여부=> bool
     캐릭터 데이터에서 작성한 정보를 받아와서 오브젝트에 붙여서 실행할 스크립트    
+
+    1. tag로 장애물 데미지 여부 확인
+
+    2. 스킬 패시브 액티브 확인
+        2-1. 패시브 : 스킬 지속 활성화 => OnCollisionEnter에서 tag확인 후 스킬 off 
+        2-2. 액티브 : 키보드 input 혹은 button.onclick확인 후 스킬 활성화 bool 
+
+    3. gaugetime 관리
+        - 초기 실행 : gaugetime 값을 10으로 설정하여 스킬 사용 가능 
+        - update에서 스킬 사용 isSkillUse + gaugetime 5초 이상시 (gaugetime + isSkillUse) 초기화 
+
         //TODO: 스킬 bool 제어 하고 그 값을 넘기면 해당 스킬 스크립트에서 스킬 사용 + 이펙트 활성화
      */
 
     public bool isDead { get; protected set; }
     public event Action OnDead;
 
+    // 버튼으로 스킬 사용
+    public Button SkillButton;
+    public Sprite ButtonSprite;
+
     // 스킬 사용해서 넘겨줘야하는 값
     public bool isSkillUse;
-
     public bool isSkillPassive;
 
     // 업데이트에서 게이지는 시간 추가 -> 스킬 사용 후 게이지값 초기화
@@ -25,6 +39,8 @@ public class PlayerControll : MonoBehaviour
     public void Awake()
     {
         gaugeTime = 10f;
+        SkillButton.gameObject.SetActive(true);
+        SkillButton.gameObject.GetComponent<Image>().sprite = ButtonSprite;
     }
 
     private void Update()
@@ -33,6 +49,7 @@ public class PlayerControll : MonoBehaviour
         {
             // 패시브일 경우 일단 계속 시간 확인해서 스킬 켜야함
             Skill_Passive_On();
+            SkillButton.enabled = false; // 패시브인 경우 버튼 활성화가 되어있으나 사용 불가
         }
         else
         {//  패시브가 아닐경우 
@@ -40,6 +57,13 @@ public class PlayerControll : MonoBehaviour
             Time_SkillUse_Clear();
             // 스킬 사용 변경 메소드
             Skill_Active_On();
+            if (isSkillUse == false && gaugeTime >=5)
+            {
+                //게이지 시간이 찼을 경우 버튼 활성화 선택가능
+                SkillButton.gameObject.SetActive(true);
+                SkillButton.onClick.AddListener(Skill_Active_Button_Use_On);
+            }
+
         }
         gaugeTime += Time.deltaTime;
       
@@ -57,6 +81,7 @@ public class PlayerControll : MonoBehaviour
         {
             gaugeTime = 0f;
             isSkillUse = false;
+            SkillButton.gameObject.SetActive(false);
             Debug.Log("스킬 사용 / 게이지 시간 초기화 완료 위치");
         }
     }
@@ -69,7 +94,16 @@ public class PlayerControll : MonoBehaviour
             Debug.Log("스킬 사용");
             isSkillUse = true;
             gaugeTime = 0f;
+            SkillButton.gameObject.SetActive(false); // 스킬 사용 후 버튼 비활성화
         }
+    }
+
+    private void Skill_Active_Button_Use_On()
+    {
+        Debug.Log("버튼으로 스킬 사용");
+        isSkillUse = true;
+        gaugeTime = 0f;
+        SkillButton.gameObject.SetActive(false); // 스킬 사용 후 버튼 비활성화
     }
 
     private void Skill_Passive_On()
@@ -77,12 +111,10 @@ public class PlayerControll : MonoBehaviour
         if (gaugeTime >= 5f)
         {
             isSkillUse = true;
+            SkillButton.gameObject.SetActive(true);
+
         }
     }
-
-
-
-
 
     // 장애물 tag통해서 캐릭터가 데미지를 받을지 안 받을지 결정
     private void OnCollisionEnter(Collision coll)
@@ -112,6 +144,7 @@ public class PlayerControll : MonoBehaviour
                 Debug.Log(isSkillPassive);
                 isSkillUse = false;
                 gaugeTime = 0f;
+                SkillButton.gameObject.SetActive(false); // 스킬1번 맞은 후 버튼 비활성화
             }
             return;
         }
